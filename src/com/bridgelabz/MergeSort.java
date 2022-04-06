b983b40d6c40f6b876761e5000c6d30e80bdd446
KiranHulawale <97278477+KiranHulawale@users.noreply.github.com>
2022-04-06 18:26:40 +0530

    Update MergeSort.java

--- src/com/bridgelabz/MergeSort.java
+++ src/com/bridgelabz/MergeSort.java
@@ -13,45 +13,5 @@ public class MergeSort {
         return merge(array, start, mid, end);
     }
 
-    public int[] merge(int [] array, int start, int mid, int end) {
-
-        int [] Left = new int[mid - start + 1];
-        int [] Right = new int[end - mid];
-
-        for(int i = 0; i < mid - start + 1; i++)
-            Left[i] = array[start + i];
-        for(int i = 0; i < end - mid; i++)
-            Right[i] = array[mid + i + 1];
-
-        int k = start, i = 0, j = 0;
-
-        while (i < Left.length && j < Right.length) {
-            if(Left[i] < Right[j])
-                array[k++] = Left[i++];
-            else
-                array[k++] = Right[j++];
-        }
-
-        while( i < Left.length) {
-            array[k++] = Left[i++];
-        }
-        while( j < Right.length) {
-            array[k++] = Right[j++];
-        }
-        return array;
-    }
-
-    public void displaySort(int [] array) {
-        for (int i : array) {
-            System.out.print(" " + i);
-        }
-        System.out.println();
-    }
-    public static void main(String[] args) {
-        MergeSort sort = new MergeSort();
-        int array[] = {40,30,10,70,50,20,60,22,44};
-
-        sort.mergeSort(array, 0, array.length - 1);
-        sort.displaySort(array);
-    }
+    
 }
