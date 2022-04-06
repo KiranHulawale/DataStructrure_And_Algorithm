77f1fef272e79cb6cbed6cb1da3a79a3ca3b9a50
Kiran <kiranhulawale123@gmail.com>
2022-04-06 11:33:32 +0530

    Added day 16 & 17 program

---
+++ src/com/bridgelabz/PrimeNumbers.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int lower = 0, upper = 1000;
+        for (int i = lower; i <= upper; i++)
+            if (isPrime (i))
+                System.out.println (i);
+    }
+    static boolean isPrime (int n)
+    {
+        int count = 0;
+
+        // 0, 1 negative numbers are not prime
+        if (n < 2)
+            return false;
+
+        // checking the number of divisors b/w 1 and the number n-1
+        for (int i = 2; i < n; i++)
+        {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
